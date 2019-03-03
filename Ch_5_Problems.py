123449dd33cfdfcc2d19f433cabb71bbc1c6804b
Brandon Ling <brandonvling@gmail.com>
2019-03-03 12:56:34 -0500

    Add exercise 5.14

--- Ch_5_Problems.py
+++ Ch_5_Problems.py
@@ -417,9 +417,30 @@ from pylab import plot, show, xlabel, ylabel, imshow, hot, xlim, ylim
 # print(sqrt(2*integral))
 
 ## Exercise 5.14
+G = 6.674 * 10 ** -11 # gravitational constant in SI units
+sigma = 100 # mass density in SI units
+N = 100 # number of points in each direction for double Gaussian quadrature
+r, w = gaussxwab(N, -5,5)
+def Fz(z):
+    def integrand(x, y, z):
+        return 1 / (x**2 + y ** 2 + z ** 2) ** 3/2
 
+    integral = 0
+    for i in range(N):
+        for j in range(N):
+            integral += w[i] * w[j] * z * integrand(r[i], r[j], z)
 
+    return G * sigma * integral
 
+zvalues = linspace(0, 10, 100)
+Fzvalues = list(map(Fz, zvalues))
+
+plot(zvalues, Fzvalues, 'o')
+xlabel('z (m)')
+ylabel('Fz (N)')
+show()
+# The force seems to drop to zero for small values of z because the integrand becomes very small for points
+# far from x,y = 0, and we don't have enough points near (0,0) when calculating the integral 
 
 
 
