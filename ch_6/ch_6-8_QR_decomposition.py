133e65b030a790d9747d22121a993968ec181859
Brandon Ling <brandonvling@gmail.com>
2019-03-08 19:00:58 -0500

    Add eigenvalue and eigenvector decomposition

--- ch_6/ch_6-8_QR_decomposition.py
+++ ch_6/ch_6-8_QR_decomposition.py
@@ -1,50 +1,89 @@
-from scipy import zeros, empty, dot, sqrt, array, copy
+from scipy import zeros, empty, dot, sqrt, array, copy, identity
 from scipy import transpose
 
-def QR_decomp(A):
+def eigen_decomp(A, error):
     """
-    returns a list containing arrays Q and R, the QR decomposition of a symmetric matrix A
-    :param A: a symmetric array
-    :return: a list of arrays
+    gives the eigenvectors and eigenvalues of a symmetric matrix A with tolerance error
+    :param A: symmetric matrix
+    :param error: positive float
+    :return: a list with first element a matrix whose columns are the eigenvectors of A, and the second element
+            a diagonal matrix with elements corresponding to the eigenvalues
     """
     N = A.shape[0]
-    Q = empty([ N, N ], float)
-    R = zeros([ N, N ], float)
 
-    # create q vectors
-    A_columns = []
-    for i in range(N):
-        A_columns.append(A[:, i])
+    def QR_decomp(A):
+        """
+        returns a list containing arrays Q and R, the QR decomposition of a symmetric matrix A
+        :param A: a symmetric array
+        :return: a list of arrays
+        """
+        N = A.shape[0]
+        Q = empty([ N, N ], float)
+        R = zeros([ N, N ], float)
 
-    def length_vector(v):
-        return sqrt(dot(v, v))
+        # create q vectors
+        A_columns = []
+        for i in range(N):
+            A_columns.append(A[:, i])
 
-    u_columns = [ copy(A_columns[0]) ]
-    q_columns = [ copy(A_columns[0]) / length_vector(A_columns[0]) ]
-    for i in range(1, N):
-        u_columns.append(copy(A_columns[i]))
-        for j in range(i):
-            u_columns[i] -= dot(q_columns[j], A_columns[i]) * q_columns[j]
-        q_columns.append(u_columns[i] / length_vector(u_columns[i]))
+        def length_vector(v):
+            return sqrt(dot(v, v))
 
-    print(q_columns)
-    # Create matrix Q
-    for i in range(N):
-        Q[:, i] = q_columns[i]
+        u_columns = [ copy(A_columns[0]) ]
+        q_columns = [ copy(A_columns[0]) / length_vector(A_columns[0]) ]
+        for i in range(1, N):
+            u_columns.append(copy(A_columns[i]))
+            for j in range(i):
+                u_columns[i] -= dot(q_columns[j], A_columns[i]) * q_columns[j]
+            q_columns.append(u_columns[i] / length_vector(u_columns[i]))
 
-    # Create matrix R
-    for i in range(N):
-        for j in range(i, N):
-            if i == j:
-                R[i, i] = length_vector(u_columns[i])
+        # Create matrix Q
+        for i in range(N):
+            Q[:, i] = q_columns[i]
+
+        # Create matrix R
+        for i in range(N):
+            for j in range(i, N):
+                if i == j:
+                    R[i, i] = length_vector(u_columns[i])
+                else:
+                    R[i, j] = dot(q_columns[i], A_columns[j])
+
+        return [ Q, R ]
+
+    def off_diags_small_enough(a):
+        def is_good_enough(x):
+            if abs(x) < error:
+                return True
             else:
-                R[i, j] = dot(q_columns[i], A_columns[j])
+                return False
 
-    return [ Q, R ]
+        for i in range(N):
+            for j in range(N):
+                if i == j:
+                    continue
+                else:
+                    if not is_good_enough(a[i, j]):
+                        return False
+        return True
+
+    V = identity(N)
+    while(not off_diags_small_enough(A)):
+        # Create matrix to store eigenvectors
+        Q, R  = QR_decomp(A)
+
+        # update A
+        A = dot(R, Q)
+
+        # update V
+        V = dot(V, Q)
+    return [ V, A ]
 
 # test
 A = array([ [1, 4, 8, 4],
             [4, 2, 3, 7],
             [8, 3, 6, 9],
             [4, 7, 9, 2] ], float)
-print(QR_decomp(A))
+[eigenvectors, eigenvalues] = eigen_decomp(A, 10 ** -6)
+print(eigenvectors)
+print(eigenvalues)
