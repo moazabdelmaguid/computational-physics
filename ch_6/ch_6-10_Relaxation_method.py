3a9b00740b68d6e57a405808d50fc24ec998e7ca
Brandon Ling <brandonvling@gmail.com>
2019-03-09 10:59:13 -0500

    Refactor to solve as function of parameter c

--- ch_6/ch_6-10_Relaxation_method.py
+++ ch_6/ch_6-10_Relaxation_method.py
@@ -3,16 +3,19 @@ from scipy import exp
 # Constants
 accuracy = 10 ** -8
 
-def f(c, x):
-    return 1 - exp(- c * x)
+def g(c):
 
-def error(x1, x2, c):
-    return (x1 - x2) / (1 - 1 / (c * exp(-c * x1)))
+    def f(x):
+        return 1 - exp(- c * x)
 
-x1 = 0.5
-x2 = f(2, x1)
+    def error(x1, x2):
+        return (x1 - x2) / (1 - 1 / (c * exp(-c * x1)))
 
-while(abs(error(x1, x2, 2)) > accuracy):
-    x1, x2 = x2, f(2, x2)
+    x1 = 0.5 # starting value
+    x2 = f(x1)
+    while(abs(error(x1, x2, c)) > accuracy):
+        x1, x2 = x2, f(c, x2)
 
-print(x2)
+    return x2
+
+
