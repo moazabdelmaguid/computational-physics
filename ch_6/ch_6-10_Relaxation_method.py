5fc614bf45679eb76c4084b1e84e58c10fa89ca3
Brandon Ling <brandonvling@gmail.com>
2019-03-10 15:33:43 -0400

    Print out number of iterations

--- ch_6/ch_6-10_Relaxation_method.py
+++ ch_6/ch_6-10_Relaxation_method.py
@@ -4,7 +4,7 @@ from pylab import plot, show, xlabel, ylabel
 accuracy = 10 ** -8
 
 def g(c):
-
+    iterations = 1
     def f(x):
         return 1 - exp(- c * x)
 
@@ -15,7 +15,8 @@ def g(c):
     x2 = f(x1)
     while(abs(error(x1, x2)) > accuracy):
         x1, x2 = x2, f(x2)
-
+        iterations += 1
+    print('number of iterations = ', iterations)
     return x2
 
 c = linspace(0, 3, 300)
